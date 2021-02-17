0dcda3e988edb36ca4c250c035bedcefbc8f6a39
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-17 17:46:51 +0330

    ignore package-lock.json and mount hero header in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Header from './components/Header'
+import HeroHeader from './components/HeroHeader'
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Header />
+      <HeroHeader />
     </React.Fragment> 
   );
 }
