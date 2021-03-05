ee7aacfbf16fc4dc3db4d95e9f3db173f04f2ce4
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-03-05 15:25:42 +0330

    configure server side stripe post responses for payment

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require('express');
 const cors = require('cors')
-
+const stripe = require('stripe')('sk_test_51IQy1ALkrJ2Fni8zL8U82LHKjywiJWHx4YQjDJKnXa3Qv0legKz1DPbyrt3fMjHmurDOTmmmUKsF0qdqJ28NOTfN00ATTS2AmN');
 const app = express();
 
 app.use(cors({
@@ -9,6 +9,29 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.post('/payments/create', async (req, res) => {
+    try {
+      const { amount, shipping } = req.body;
+      const paymentIntent = await stripe.paymentIntents.create({
+        shipping,
+        amount,
+        currency: 'usd'
+      });
+  
+      res
+        .status(200)
+        .send(paymentIntent.client_secret);
+  
+    } catch (err) {
+      res
+        .status(500)
+        .json({
+          statusCode: 500,
+          message: err.message
+        });
+    }
+  })
+  
 app.get('/', (req, res) => {
     res
     .status(404)
