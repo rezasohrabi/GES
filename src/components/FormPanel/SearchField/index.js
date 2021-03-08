0a14260339a3c1ba1fb463011f1c957f39a194d6
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-03-08 19:04:39 +0330

    add search component

---
+++ src/components/FormPanel/SearchField/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Search } from '@material-ui/icons';
+import { makeStyles, InputBase, fade } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.primary.light, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.primary.light, 0.25),
+            transition: theme.transitions.create(
+                ['background-color'],
+                { duration: theme.transitions.duration.complex }
+                ),
+        },
+    },
+    searchInput: {
+        width: '100%',
+    },
+    searchIcon: {
+        margin: theme.spacing(1),
+    },
+    
+}));
+
+const SearchField = ({placeholder, fullWidth, ...otherProps}) => {
+    const classes = useStyles();
+    return (
+        <div className={fullWidth
+        ?{
+            root: classes.root,
+            fullWidth: classes.searchInput
+        }  
+        :
+        classes.root 
+        }>
+            <Search color='inherit' className={classes.searchIcon}/>
+            <InputBase
+              placeholder={placeholder}
+              className={classes.searchInput}
+              inputProps={{ 'aria-label': 'search' }}
+              {...otherProps}
+            />
+        </div>
+    );
+};
+
+export default SearchField;
