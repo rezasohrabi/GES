f4b9816915a23cc063f1d214cff94587ccc48bbd
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-03-19 23:47:35 +0330

    add some fields for AddProduct component

--- src/components/AddProduct/index.js
+++ src/components/AddProduct/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MenuItem, TextField, Select, InputLabel, Button, FormControl, Typography, makeStyles } from '@material-ui/core';
 import { addNewProductStart } from '../../redux/Products/products.actions';
 import CKEditor from 'ckeditor4-react';
@@ -14,24 +14,77 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+const mapState = ({categoryData}) => ({
+    categories: categoryData.categories,
+});
+
+const configColourItems = [
+    {value: 'white', label: 'White'},
+    {value: 'black', label: 'Black'},
+    {value: 'red', label: 'Red'},
+    {value: 'green', label: 'Green'},
+    {value: 'grey', label: 'Grey'},
+    {value: 'blue', label: 'Blue'},
+    {value: 'pink', label: 'Pink'},    
+];
+
+const configSizeItems = [
+    {value: 'XS', label: 'X-Small'},
+    {value: 'S', label: 'Small'},
+    {value: 'M', label: 'Medium'},
+    {value: 'L', label: 'Large'},
+    {value: 'XL', label: 'X-Large'},
+    {value: '2XL', label: 'XX-Large'},
+]
+
+const configBrandItems = [
+    {value: 'nike', label: 'Nike'},
+    {value: 'adidas', label: 'Adidas'},
+    {value: 'levis', label: 'Levis'},
+    {value: 'gussi', label: 'Gucci'},
+    {value: 'poloRalph', label: 'Polo Ralph Lauren'},
+    {value: 'versace', label: 'Versace'},
+    {value: 'calvin', label: 'Calvin Klein'},
+    {value: 'american', label: 'American Eagle'},
+    {value: 'aeropostale', label: 'Aeropostale'},
+    {value: 'victoria', label: 'Abercrombie & Fitch'},
+    {value: 'puma', label: 'Puma'},
+    {value: 'vans', label: 'Vans'},
+    {value: 'louis', label: 'Louis Vuitton'},
+    {value: 'lacoste', label: 'Lacoste'},
+    {value: 'tommy', label: 'Tommy Hilfiger'},
+];
+
 const AddProduct = props => {
     const [productName, setProductName] = useState('');
     const [productMenu, setProductMenu] = useState('');
     const [productCategory, setProductCategory] = useState('');
     const [productThumbnail, setProductThumbnail] = useState('');
     const [productPrice, setProductPrice] = useState(); 
+    const [productQuantity, setProductQuantity] = useState();
+    const [productDesc, setProductDesc] = useState('');
+    const [productBrand, setProductBrand] = useState('');
+    const [productColour, setProductColour] = useState([]);
+    const [productSize, setProductSize] = useState([]);
     const dispatch = useDispatch();
+    const { categories } = useSelector(mapState);
     const classes = useStyles();
 
     const handleSubmit = e => {
         e.preventDefault();
+        const imageUrls = productThumbnail.split('+');
         dispatch(
             addNewProductStart({
                 productName,
+                productMenu,
                 productCategory,
-                productThumbnail,
+                productThumbnail: imageUrls,
                 productPrice,
+                productQuantity,
                 productDesc,
+                productBrand,
+                productColour,
+                productSize,
             })
         );
         resetForm();
@@ -39,9 +92,9 @@ const AddProduct = props => {
 
     const resetForm = () => {
         setProductName('');
-        setProductCategory('men');
+        setProductCategory('');
         setProductThumbnail('');
-        setProductPrice(0);
+        setProductPrice();
         setProductDesc('');
     }
 
@@ -77,6 +130,77 @@ const AddProduct = props => {
                     <MenuItem value='baby'>Baby</MenuItem>
                 </Select>
             </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel >Which Category?</InputLabel>
+                <Select
+                value={productCategory}
+                onChange={e => setProductCategory(e.target.value)}
+                required
+                >
+                    <MenuItem value=''>Select Category</MenuItem>
+                    {categories
+                    .filter(cate => cate.categoryMenu === productMenu)
+                    .map(filteredCate => <MenuItem 
+                        value={filteredCate.categoryName}
+                        >{filteredCate.categoryName}
+                        </MenuItem>
+                        )
+                    }
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel >Which Brand?</InputLabel>
+                <Select
+                value={productBrand}
+                onChange={e => setProductBrand(e.target.value)}
+                required
+                >
+                    <MenuItem value=''>Select Brand</MenuItem>
+                    {configBrandItems
+                    .map(brand => <MenuItem 
+                        value={brand.label}
+                        >{brand.label}
+                        </MenuItem>
+                        )
+                    }
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel >Available Colours</InputLabel>
+                <Select
+                value={productColour}
+                onChange={e => setProductColour(e.target.value)}
+                multiple
+                required
+                >
+                    <MenuItem value=''>Select Available Colours</MenuItem>
+                    {configColourItems
+                    .map(colour => <MenuItem 
+                        value={colour.label}
+                        >{colour.label}
+                        </MenuItem>
+                        )
+                    }
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel >Available Sizes</InputLabel>
+                <Select
+                value={productSize}
+                onChange={e => setProductSize(e.target.value)}
+                multiple
+                required
+                >
+                    <MenuItem value=''>Select Available Sizes</MenuItem>
+                    {configSizeItems
+                    .map(size => <MenuItem 
+                        value={size.label}
+                        >{size.label}
+                        </MenuItem>
+                        )
+                    }
+                </Select>
+            </FormControl>
             <TextField
             name='productThumbnail'
             type='Url'
@@ -85,6 +209,7 @@ const AddProduct = props => {
             onChange={ e => setProductThumbnail(e.target.value)}
             margin='normal'
             fullWidth
+            placeholder='url1 + url2 + ... + urln '
             />
             <TextField
             name='productPrice'
@@ -92,6 +217,15 @@ const AddProduct = props => {
             label='Price'
             value={productPrice}
             onChange={ e => setProductPrice(e.target.value) }
+            margin='normal'
+            fullWidth
+            />
+            <TextField
+            name='productQuantity'
+            type='number'
+            label='Quantity'
+            value={productQuantity}
+            onChange={ e => setProductQuantity(e.target.value) }
             margin='normal'
             fullWidth
             />
